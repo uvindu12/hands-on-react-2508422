8b298e2e5223ac1c43b8ae2149bc67c90dfa9876
S A U Kithmina <125674071+uvindu12@users.noreply.github.com>
2024-10-29 13:42:58 +0000

    git push -for

--- src/components/ListCast.jsx
+++ src/components/ListCast.jsx
@@ -1,31 +1,34 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect} from "react";
 
-export default () => {
+export default ({onChoice}) => 
+{
   const [cast, setCast] = useState([]);
 
-  async function fetchCast() {
-    const response = await fetch('cast.json');
-    setCast(await response.json());
+  async function fetchCast ()
+  {
+    const response = await fetch ('cast.json');
+    setCast (await response.json ());
   }
 
-  useEffect(() => {
-    fetchCast();
-  });
+  useEffect (() =>
+{
+  fetchCast ();
+});
 
-  return (
-    <div style={{
-      display: "grid",
-      gridTemplateColumns: `repeat(auto-fit, minmax(90px, 1fr))`,
-      gap: `1rem`,
-      marginBottom: '1rem'
-    }}>
-      {
-        cast.map(member => (
-          <a key={member.id} data-tooltip={member.name}>
-            <img src={`images/${member.slug}_tn.svg`} alt={member.name} />
-          </a>
-        ))
-      }
-    </div>
-  )
+return (
+  <div style = {{
+    display:"grid",
+    gridTemplateColumns: `repeat(auto-fit, minmax(90px, 1fr))`,
+    gap:`1rem`,
+    marginBottom: `1rem`
+  }}>
+    {
+      cast.map(member => (
+      <a onClick={() => {onChoice(member)}} key={member.id} data-tooltip={member.name}>
+        <img src={`images/${member.slug}_tn.svg`} alt={member.name}/>
+      </a>
+      ))
+    }
+  </div>
+)
 }
