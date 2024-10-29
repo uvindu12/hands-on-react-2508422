035c0ea587f62aae0ef31c97f6b382832c69d095
S A U Kithmina <125674071+uvindu12@users.noreply.github.com>
2024-10-29 07:36:30 +0000

    git push - for

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -4,6 +4,8 @@ export default () =>
 {
   const [count, setCount] = useState(0)
   return (
-    <button className="outline" onClick={() => setCount(count+ 1)}>Supported {count}</button>
+    <button className="outline" onClick={() => setCount(count+ 1)}>
+      {count === 0 ? "Click to Support" : `Supported ${count} times`}
+    </button>
   )
 }
